cf601f1e90f62c16424fac99814d6bb90626b087
Mario Melo Núñez <melomario57@gmail.com>
2024-06-14 17:14:18 -0600

    Se implemento funcion al boton guardar

--- script/script.js
+++ script/script.js
@@ -2,35 +2,47 @@ let popUp = document.querySelector(".popup");
 let openButton = document.querySelector(".profile__edit-button");
 let closeButton = document.querySelector(".popup__button-cross");
 let saveButton = document.querySelector(".popup__form-button");
-let likeButton = document.querySelector(".cards__hearth-button");
+let likeButton = document.querySelectorAll(".cards__hearth-button");
 
 function toggleForm() {
   popUp.classList.toggle("popup_opened");
-}
-
-function toggleFormButton() {
-  likeButton.classList.toggle("cards__hearth-button_active");
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
 }
 
 openButton.addEventListener("click", toggleForm);
 closeButton.addEventListener("click", toggleForm);
-saveButton.addEventListener("click", toggleForm);
-likeButton.addEventListener("click", toggleFormButton);
 
+function save() {
+  popUp.classList.toggle("popup_opened");
+}
+
+saveButton.addEventListener("click", save);
 /* Editar nombre y acerca de mi en el formulario */
 
 let formElement = document.querySelector(".popup__form");
 
-function handleProfileFormSubmit(evt) {
-  evt.preventDefault();
-  profileName.textContent = inputName.value;
-  profileJob.textContent = inputJob.value;
-}
-
 let profileName = document.querySelector(".profile__title");
 let profileJob = document.querySelector(".profile__subtitle");
 
-let inputName = document.querySelector("#name").value;
-let inputJob = document.querySelector("#about").value;
+let inputName = document.getElementById("name");
+let inputJob = document.getElementById("about");
+
+inputName.value = profileName.textContent;
+inputJob.value = profileJob.textContent;
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  let nameValue = inputName.value;
+  let jobValue = inputJob.value;
+
+  profileName.textContent = nameValue;
+  profileJob.textContent = jobValue;
+
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
+  popUp.classList.toggle(".popup_opened");
+}
 
 formElement.addEventListener("submit", handleProfileFormSubmit);
